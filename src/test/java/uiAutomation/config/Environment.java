91db007d4c52ee846a0bf94c7ee56af189daf479
shevanr <shevanaufalrifqi28@gmail.com>
2025-02-23 18:49:46 +0700

    adjust environment for github actions

--- src/test/java/uiAutomation/config/Environment.java
+++ src/test/java/uiAutomation/config/Environment.java
@@ -12,7 +12,7 @@ public class Environment {
     private Environment(){
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         String baseUrl = "https://www.demoblaze.com/";
